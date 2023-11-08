074fbf5e14b458a9f70cb6ce074b545407916118
Autumn Han <autumn9894@gmail.com>
2023-11-08 10:22:41 -0800

    took isPrime() out of largestPrime() and set in the global scope

--- projectEuler/problem3.js
+++ projectEuler/problem3.js
@@ -1,20 +1,21 @@
 // prompt: what is the largest prime factor of the number 600851475143
 
-function largestPrime(num) {
-  // isPrime checks to see if number argument is a prime integer by checking to see if the integer has more than one and itself as factors
-  function isPrime(wholeNum) {
-    var count = 0;
-    for (let i = 2; i < wholeNum / 2; i++) {
-      if (wholeNum % i === 0) {
-        count++;
-      }
-    }
-    if (count !== 0) {
-      return false;
-    } else {
-      return true;
+// isPrime checks to see if number argument is a prime integer by checking to see if the integer has more than one and itself as factors
+function isPrime(wholeNum) {
+  var count = 0;
+  for (let i = 2; i < wholeNum / 2; i++) {
+    if (wholeNum % i === 0) {
+      count++;
     }
   }
+  if (count !== 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function largestPrime(num) {
   for (let i = 2; i < num / 2; i++) {
     // check if factor is a whole number
     if (num % i === 0) {
