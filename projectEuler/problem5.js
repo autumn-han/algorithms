0ad9efc4d313e937dcddce7c645bf11d83eda368
Autumn Han <autumn9894@gmail.com>
2023-11-08 20:01:09 -0800

    wrote out smallestMult(), with isDivisible() as a helper function

---
+++ projectEuler/problem5.js
@@ -0,0 +1,28 @@
+// prompt: find the smallest positive number that is evenly divisible by all of the numbers from 1 to 20
+
+// helper function for checking if integer is divisible by all numbers from 1 to 20
+function isDivisible(int) {
+  var count = 0;
+  for (let i = 1; i <= 20; i++) {
+    if (int % i === 0) {
+      count++;
+    }
+  }
+  if (count === 20) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// main function
+function smallestMult() {
+  var i = 20;
+  do {
+    isDivisible(i);
+    i += 2;
+  } while (isDivisible(i) === false);
+  return i;
+}
+
+console.log(smallestMult());
