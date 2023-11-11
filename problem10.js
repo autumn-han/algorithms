10416fe07e5b2e2a8992b8bae84143374c1b4c13
Autumn Han <autumn9894@gmail.com>
2023-11-11 10:12:50 -0800

    changed while loop to a for-loop to try and reduce run time

--- problem10.js
+++ problem10.js
@@ -21,13 +21,9 @@ function isPrime(a) {
 // main function
 function primeSum() {
   var a = 0;
-  let i = 2;
-  while (i < 2000000) {
+  for (let i = 2; i < 2000000; i++) {
     if (isPrime(i) === true) {
       a += i;
-      i++;
-    } else {
-      i++;
     }
   }
   return a;
