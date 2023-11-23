91a949755dd21be24248bb7d4b505fc0d2f3ce1c
Autumn Han <autumn9894@gmail.com>
2023-11-23 09:29:32 -0800

    finished countLogs() in logEntry problem

--- leetCode/logEntry.js
+++ leetCode/logEntry.js
@@ -1,4 +1,8 @@
-// input
+// prompt: given an array of log entries (logs) that store a date and userID as a single string, return a sequence of arrays
+// that show the date and the total number of unique userIDs for that date. You can assume that the dates will always be given
+// in order of earliest to latest.
+
+// test cases
 const logs = [
   "2023-10-01: User1",
   "2023-10-02: User1",
@@ -7,12 +11,25 @@ const logs = [
   "2023-10-02: User2",
   "2023-10-03: User3",
   "2023-10-03: User3",
-  "2023-10-03: User1",
 ];
 // expected output
 // ["2023-10-01", 1]
 // ["2023-10-02", 2]
 // ["2023-10-03", 1]
+const logsTwo = [
+  "2023-10-01: User1",
+  "2023-10-02: User1",
+  "2023-10-02: User2",
+  "2023-10-02: User3",
+  "2023-10-02: User3",
+  "2023-10-03: User1",
+  "2023-10-03: User2",
+  "2023-10-03: User3",
+];
+// expected output
+// ["2023-10-01", 1]
+// ["2023-10-02", 3]
+// ["2023-10-03", 3]
 
 // TANNER'S METHOD
 // store data in an object
@@ -28,12 +45,19 @@ function countUsersForEachDate(arr) {
   let count = 0;
   let returnArr = [];
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i][0] === date) {
+    if (i === arr.length - 1) {
+      if (arr[i][0] !== date) {
+        returnArr.push([date, count]);
+        returnArr.push([arr[i][0], 1]);
+      } else {
+        count++;
+        returnArr.push([date, count]);
+      }
+    } else if (arr[i][0] === date) {
       count++;
-    } else if (i === arr.length - 1) {
-      returnArr.push([arr[i][0], count]);
     } else {
       returnArr.push([date, count]);
+
       date = arr[i][0];
       count = 1;
     }
@@ -52,3 +76,4 @@ function countLogs(logs) {
 }
 
 countLogs(logs);
+countLogs(logsTwo);
