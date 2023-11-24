81c777b250f4a5dea2972d70191cfa68a84e852f
Autumn Han <autumn9894@gmail.com>
2023-11-24 09:20:03 -0800

    changed comments and variable data types for problems 3 and 4; optimized largestVal() in problem 4

--- projectEuler/problem4.js
+++ projectEuler/problem4.js
@@ -1,16 +1,9 @@
 // prompt: find the largest palindrome made from the product of two 3-digit numbers
 
-// determine if integer is a palindrome
 function isPalindrome(num) {
-  var count = 0;
-  let numArr = String(num).split("").map(Number);
-  let mid = Math.floor(numArr.length / 2);
-  for (let i = 0; i < mid; i++) {
-    if (numArr[i] === numArr[numArr.length - 1 - i]) {
-      count++;
-    }
-  }
-  if (count === mid) {
+  let numString = num.toString();
+  let reversedNumString = numString.split("").reverse().join("");
+  if (reversedNumString === numString) {
     return true;
   } else {
     return false;
@@ -19,17 +12,18 @@ function isPalindrome(num) {
 
 // find largest value in unsorted array
 function largestVal(arr) {
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[0] < arr[i]) {
-      arr[0] = arr[i];
+  let max = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
     }
   }
-  return arr[0];
+  return max;
 }
 
 // main function
 function largestPal() {
-  var palArr = [];
+  let palArr = [];
   for (let i = 999; i > 99; i--) {
     for (let j = 999; j > 99; j--) {
       let k = i * j;
