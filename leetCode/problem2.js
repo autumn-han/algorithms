6a39f1f1362c4d58ae74da5dd55f45dafa9ae8f0
Autumn Han <autumn9894@gmail.com>
2023-11-27 12:31:52 -0800

    built out Node and Linked List classes; started hard-coding the logic for addTwoNumbers()

--- leetCode/problem2.js
+++ leetCode/problem2.js
@@ -8,10 +8,60 @@
 // output: [7. 0, 8]
 // explanation: 342 + 465 = 807
 
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  add(data) {
+    const node = new Node(data);
+    node.next = this.head;
+    this.head = node;
+  }
+}
+
+// instantiate l1 and add data
+const l1 = new LinkedList();
+l1.add(3);
+l1.add(4);
+l1.add(2);
+// instantiate l2 and add data
+const l2 = new LinkedList();
+l2.add(4);
+l2.add(6);
+l2.add(5);
+
+// main function
 function addTwoNumbers(l1, l2) {
-  // take the last node of l1 and move through to head of linked list - store in as variable
-  // do the above with the l2
-  // find sum of tempL1 and tempL2
-  // store sum in reverse into a linked list l3
+  // take the last node of l1 and move through to head of linked list - store as a string variable
+  // reverse string
+  // concatenate the string with each element in the linked list in a while loop
+  // ^^^ do the above with l2 in a separate while loop ^^^
+  // convert strL1 and strL2 into integers (intL1 and intL2)
+  // find sum of intL1 and intL2 and store as string
+  // reverse sum
+  // split sum into an array
+  // instantiate l3
+  // for each value in array, convert to integer and add to linked list l3
   // return l3
+  let strL1;
+  let strL2;
+  while (l1.head !== null) {
+    strL1 = strL1.concat(l1.head.data);
+    l1.head = l1.head.next;
+  }
+  while (l2.head !== null) {
+    strL2 = strL2.concat(l2.head.data);
+    l2.head = l2.head.next;
+  }
+  reversedIntL1 = strL1.split("").reverse().join("").Number();
+  reversedIntL2 = strL2.split("").reverse().join("").Number();
+  let strSum = toString(reversedIntL1 + reversedIntL2);
+  let reversedStrSum = strSum.reverse();
 }
