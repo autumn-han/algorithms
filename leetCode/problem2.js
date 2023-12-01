44f9f2307e9f18a63a0c9c21e44cc79b9d76893e
Autumn Han <autumn9894@gmail.com>
2023-12-01 10:43:07 -0800

    changed addTwoNumbers() to fit the parameters of leet code; took notes from previous solns

--- leetCode/problem2.js
+++ leetCode/problem2.js
@@ -1,65 +1,43 @@
-// prompt: You are given two non-empty linked lists representing two non-negative integers.
+// --- prompt ---
+// You are given two non-empty linked lists representing two non-negative integers.
 // The digits are stored in reverse order, and each of their nodes contains a single digit.
 // Add the two numbers and return the sum as a linked list.
 // You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
-// test case
+// --- definition for singly-linked list ---
+// function ListNode(val, next) {
+//   this.val = (val===undefined ? 0 : val)
+//   this.next = (next===undefined ? null : next)
+// }
+
+// --- test case ---
 // input: l1 = [2, 4, 3], l2 = [5, 6, 4]
 // output: [7. 0, 8]
 // explanation: 342 + 465 = 807
 
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
-  }
-}
-
-class LinkedList {
-  constructor() {
-    this.head = null;
-  }
-  add(data) {
-    const node = new Node(data);
-    node.next = this.head;
-    this.head = node;
-  }
-}
-
-// instantiate l1 and add data
-const l1 = new LinkedList();
-l1.add(3);
-l1.add(4);
-l1.add(2);
-// instantiate l2 and add data
-const l2 = new LinkedList();
-l2.add(4);
-l2.add(6);
-l2.add(5);
-
-// main function
 function addTwoNumbers(l1, l2) {
-  const lSum = new LinkedList();
-  let strL1 = "";
-  let strL2 = "";
-  while (l1.head !== null) {
-    strL1 = strL1.concat(l1.head.data);
-    l1.head = l1.head.next;
-  }
-  while (l2.head !== null) {
-    strL2 = strL2.concat(l2.head.data);
-    l2.head = l2.head.next;
-  }
-  let reversedIntL1 = Number(strL1.split("").reverse().join(""));
-  let reversedIntL2 = Number(strL2.split("").reverse().join(""));
-  let sum = reversedIntL1 + reversedIntL2;
-  let strSum = sum.toString();
-  const strSumArr = strSum.split("");
-  for (let i = 0; i < strSumArr.length; i++) {
-    const val = Number(strSumArr[i]);
-    lSum.add(val);
-  }
-  return lSum;
-}
+  let tempHead = new ListNode(0); // initializing new node
+  let current = tempHead; // initializing pointer to new node
+  let carryOver = 0; // initializing variable to store any carry over from sum operations
 
-console.log(addTwoNumbers(l1, l2));
+  while (l1 || l2) {
+    const x = l1 ? l1.val : 0;
+    const y = l2 ? l2.val : 0;
+    const sum = x + y + carryOver; // while l1 and l2 are not null perform sum operations for each corresponding node val
+
+    carryOver = Math.floor(sum / 10); // calculate carry over for the next iteration
+    current.next = new ListNode(sum % 10); // create new node and assign remainder from sum / 10 as val
+    current = current.next; // move pointer to the next node
+
+    // move to the next node in each given linked list, if not null
+    if (l1) l1 = l1.next;
+    if (l2) l2 = l2.next;
+  }
+
+  // if there is carry over after running while loop, assign val to new node
+  if (carryOver > 0) {
+    currentHead.next = new ListNode(carryOver);
+  }
+
+  return tempHead.next; // return the node after tempHead, which is the head of the result linked list
+}
