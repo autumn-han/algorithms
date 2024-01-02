09ff7534ea4e6e7ddbd11062f109c6fab831b3fc
Autumn Han <autumn9894@gmail.com>
2024-01-01 18:13:20 -0800

    made requested changes from PR review

--- projectEuler/problem6.js
+++ projectEuler/problem6.js
@@ -1,18 +1,17 @@
-// prompt: find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum
+// prompt: for the first one hundred natural numbers, find the difference between the sum of the squares of and the square of the sum
 
 function sumSquareDiff() {
   let i = 1;
-  var a = 0;
-  var b = 0;
+  let sumOfAllNums = 0;
+  let sumOfAllSqrdValues = 0;
   do {
-    // set a to store the sum of all values between 1 and 100
-    a += i;
-    // set b to store the sum of values squared between 1 and 100
-    b += i ** 2;
+    // set sumOfAllNums to store the sum of all values between 1 and 100
+    sumOfAllNums += i;
+    // set sumOfAllSqrdValues to store the sum of values squared between 1 and 100
+    sumOfAllSqrdValues += i ** 2;
     // increment i to iterate through
     i++;
   } while (i <= 100);
   // find the difference between the two sums
-  let diff = a ** 2 - b;
-  return diff;
+  return sumOfAllNums ** 2 - sumOfAllSqrdValues;
 }
